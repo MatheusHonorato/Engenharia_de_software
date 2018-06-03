378a7cb28c757a9e1891eb46da485a7c34ac034b
matheus_felipe_paixao_honorato <matheuspaixaohonorato@gmail.com>
2018-06-03 16:51:36 -0300

    plus

--- integra/resources/views/admin/cadastro/cursos/index.blade.php
+++ integra/resources/views/admin/cadastro/cursos/index.blade.php
@@ -17,7 +17,7 @@
                               <th>Editar</th>
                               <th>Remover</th>
                               <th><a href="{{ route('admin.home.cadastro.categorias.index') }}"><button type="submit" class="btn btn-primary pull-right">Categorias</button></a></th>
-                              <th><a href="{{ route('admin.home.cadastro.cursos.criar') }}"><button type="submit" class="btn btn-success pull-right"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>Novo</button></a></th>
+                              <th><button href="#myModalsave" class="btn btn-success pull-right" data-toggle="modal"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>Novo</button></th>
                             </tr>
                         </thead>
                         <tbody>
@@ -58,7 +58,52 @@
             </div>
         </div>
     </div>
+    <div class="modal fade" id="myModalsave" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+                    <h4 class="modal-title">Novo</h4>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('admin.home.cadastro.cursos.store') }}" method="POST" id="save">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">                            
+                        <div class="form-group">
+                            <input type="text" name="name" id="name" class="form-control" placeholder="Nome" required="" autofocus="">
+                        </div>
+                        <div class="form-group">
+                            <select name="area" class="form-control">
+                            @foreach($areas as $area)
+                                <option value="{{ $area->id }}">{{ $area->name }}</option>
+                            @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <select name="tipo" class="form-control">
+                            @foreach($tipos as $tipo)
+                                <option value="{{ $tipo->id }}">{{ $tipo->name }}</option>
+                            @endforeach
+                            </select>
+                        </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Sair</button>
+                    <button type="button" class="btn btn-primary" onclick="update('save');">Salvar</button>
+                </div>
+                    </form>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
 </div>
 @endsection
+
+<script type="text/javascript">
+    function update($id)
+
+    {
+
+        document.getElementById($id).submit();
+
+    }
+</script>
 
 @extends('admin.layouts.partials.footer')
