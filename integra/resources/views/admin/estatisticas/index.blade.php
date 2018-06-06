fa865b3540d0bfd009163c156af7adb8c8268a22
matheus_felipe_paixao_honorato <matheuspaixaohonorato@gmail.com>
2018-06-06 18:55:47 -0300

    plus

--- integra/resources/views/admin/estatisticas/index.blade.php
+++ integra/resources/views/admin/estatisticas/index.blade.php
@@ -2,19 +2,25 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading">Inicio - Estatísticas</div>
 
                 <div class="panel-body">
-                    <div style="max-width: 300px;">
-                    <canvas id="myChart"></canvas>
+                    <h4>Ranking Alunos Habilidades, Ranking avaliação empresas, Estatísticas Matchs(quantidade de matchs por mes)</h4>
+                    <div style="max-width: 500px;" class="col-md-5 left">
+                        <canvas id="skills"></canvas>
+                    </div>
+
+                    <div style="max-width: 500px;" class="col-md-5 col-md-offset-2 right">
+                        <canvas id="areas"></canvas>
+                    </div>
                 </div>
         
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
 
                 <script type="text/javascript">
-                    new Chart(document.getElementById("myChart"), {
+                    new Chart(document.getElementById("skills"), {
                         type: 'pie',
                         data: {
                           labels: ["mysql", "java", "jquery", "html", "comunicação"],
@@ -28,6 +34,26 @@
                           title: {
                             display: true,
                             text: 'Habilidades alunos universidade'
+                          }
+                        }
+                    });
+                </script>
+
+                <script type="text/javascript">
+                    new Chart(document.getElementById("areas"), {
+                        type: 'pie',
+                        data: {
+                          labels: ["Ciencia da computação", "Engenharia", "Letras", "Licenciatura", "Comunicação"],
+                          datasets: [{
+                            label: "Skills",
+                            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+                            data: [2478,5267,734,784,433]
+                          }]
+                        },
+                        options: {
+                          title: {
+                            display: true,
+                            text: 'Áreas alunos universidade'
                           }
                         }
                     });
