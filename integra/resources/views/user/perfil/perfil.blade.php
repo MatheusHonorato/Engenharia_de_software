964ad89b54024f210079960d2a55a17282c1a4d3
matheus_felipe_paixao_honorato <matheuspaixaohonorato@gmail.com>
2018-06-09 22:47:29 -0300

    plus

--- integra/resources/views/user/perfil/perfil.blade.php
+++ integra/resources/views/user/perfil/perfil.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Cadastro</div>
+                <div class="panel-heading">Perfil</div>
                 <div class="panel-body">
                      <form class="form-horizontal" role="form" method="POST" action="{{ route('admin.home.perfil.index.update') }}">
                         {{ csrf_field() }}
@@ -23,39 +23,38 @@
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail</label>
+                        <div class="form-group">
+                            <label for="name" class="col-md-4 control-label">Período</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" placeholder="E-Mail" value="{{ $user->email }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                              <select class="form-control" name="periodo">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                              </select>
+                          </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Senha</label>
+                        <div class="form-group">
+                            <label for="name" class="col-md-4 control-label">Telefone</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                                <input class="form-control" type="text" name="phone" placeholder="Telefone">
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirmar Senha</label>
+                            <label for="name" class="col-md-4 control-label">Idade</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                <input class="form-control" type="text" name="idade" placeholder="Idade">
                             </div>
                         </div>
 
